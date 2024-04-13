08f41de96a6fc0ae9a42cda83fd022b69fc44db1
LeonWu610 <leia80020@gmail.com>
2024-04-13 19:28:26 +0800

    change of port

--- src/api/api.js
+++ src/api/api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import pinyin from 'pinyin'
 
 const instance = axios.create({
-  baseURL: 'http://localhost:4000'
+  baseURL: 'http://localhost:3000'
 })
 
 export async function fetchBanner () {
